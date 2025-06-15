6c5f8df832c156b8e72c3ffc2a5f012a93adc820
bryen <masfebriansyah2004@gmail.com>
2025-06-16 01:26:56 +0700

    Update Login, Sidebar

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -41,10 +41,16 @@ const handleSubmit = async (e) => {
   return (
     <div className="login-container">
       <div className="login-logo">
-        <div className="logo-text">
-            <span>SECURITY</span>
-            <span>REPORT</span>
-        </div>
+      <div className="logo-circle">
+        <svg className="shield-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M12 2L3 7V11C3 16.55 6.84 21.74 12 23C17.16 21.74 21 16.55 21 11V7L12 2Z" stroke="white" strokeWidth="2" strokeLinejoin="round"/>
+          <path d="M9 12L11 14L15 10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
+      </div>
+      <div className="logo-text">
+        <span>SECURITY</span>
+        <span>REPORT</span>
+      </div>
     </div>
 
       
